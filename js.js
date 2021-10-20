9b0113635eb6d27371cf6d5db3a74b286401ddb9
alinaruchkina <alina.kartyschova@gmail.com>
2021-10-20 22:11:16 +0300

    add arcs and circles

--- js.js
+++ js.js
@@ -1,21 +1,32 @@
 let canvas = document.getElementById('c1');
 let ctx = canvas.getContext('2d');
-var myColor = 'red';
-document.getElementById('color').oninput = function () {
-  myColor = this.value;
-};
+let pi = Math.PI;
 
-let a;
+ctx.beginPath();
+ctx.lineWidth = 5;
+ctx.strokeStyle = 'red';
+ctx.fillStyle = 'yellow';
+ctx.arc(150, 100, 75, 0, 2 * pi, false);
+ctx.stroke();
+ctx.fill();
 
-canvas.onmousedown = function (event) {
-  canvas.onmousemove = function (event) {
-    let x = event.offsetX;
-    let y = event.offsetY;
-    ctx.fillRect(x - 5, y - 5, 10, 10);
-    ctx.fillStyle = myColor;
-    ctx.fill();
-  };
-  canvas.onmouseup = function () {
-    canvas.onmousemove = null;
-  };
+ctx.beginPath();
+ctx.lineWidth = 5;
+ctx.strokeStyle = 'green';
+ctx.fillStyle = 'pink';
+ctx.arc(270, 100, 75, 0, 2 * pi, false);
+ctx.stroke();
+ctx.fill();
+
+ctx.clearRect(0, 0, 400, 200);
+
+canvas.onmousemove = function (event) {
+  let x = event.offsetX;
+  let y = event.offsetY;
+  ctx.clearRect(0, 0, 400, 200);
+  ctx.beginPath();
+  let radius = Math.pow(Math.pow(x - 200, 2) + Math.pow(y - 100, 2), 0.5);
+  ctx.arc(200, 100, Math.abs(x - 200), 0, 2 * pi, false);
+  ctx.stroke();
+  ctx.fill();
 };
