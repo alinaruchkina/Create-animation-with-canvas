86a86a21f415e29429f20140c449fab5e1e33cc2
alinaruchkina <alina.kartyschova@gmail.com>
2021-10-20 21:03:21 +0300

    add the line

--- js.js
+++ js.js
@@ -1,13 +1,31 @@
 var canvas = document.getElementById('c1');
 var ctx = canvas.getContext('2d');
-ctx.fillStyle = 'red';
-ctx.fillRect(100, 50, 150, 75);
-ctx.fillStyle = 'blue';
-ctx.fillRect(150, 100, 100, 50);
-ctx.clearRect(0, 0, 400, 200);
-ctx.strokeStyle = 'green';
-ctx.lineWidth = '10';
-ctx.rect(5, 10, 100, 100);
+
+ctx.beginPath();
+ctx.strokeStyle = 'red';
+ctx.lineWidth = '5';
+ctx.moveTo(100, 50);
+ctx.lineTo(150, 150);
 ctx.stroke();
-ctx.fillStyle = 'orange';
+
+ctx.beginPath();
+ctx.strokeStyle = 'blue';
+ctx.lineWidth = '20';
+ctx.moveTo(200, 50);
+ctx.lineTo(300, 50);
+ctx.lineTo(300, 100);
+ctx.lineCap = 'square';
+ctx.stroke();
+ctx.clearRect(0, 0, 400, 200);
+
+ctx.beginPath();
+ctx.moveTo(50, 150);
+ctx.lineTo(150, 50);
+ctx.lineTo(200, 150);
+ctx.lineTo(50, 150);
+ctx.lineWidth = '5';
+ctx.lineCap = 'butt';
+ctx.fillStyle = 'yellow';
+
+ctx.stroke();
 ctx.fill();
